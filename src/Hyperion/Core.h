fb97cad0e50221a7c2c3db95992aa0343bc54258
SkillerRaptor <skillerraptor@protonmail.com>
2020-12-16 00:02:00 +0100

    [General] Updating ASSERT with ifdef

--- src/Hyperion/Core.h
+++ src/Hyperion/Core.h
@@ -22,4 +22,8 @@ constexpr Ref<T> CreateRef(Args&& ... args)
 
 #include "Utilities/Log.h"
 
+#ifdef _DEBUG
 #define HP_ASSERT(x, ...) { if(!(x)) { HP_FATAL("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+#define HP_ASSERT(x, ...)
+#endif
